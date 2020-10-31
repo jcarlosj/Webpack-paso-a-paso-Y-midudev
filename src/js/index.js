9d15ea76ace4706470c890f5fcb1caf870a64bdb
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-31 16:08:05 -0500

    [+] NPM: Instala dependencias @babel/plugin-syntax-optional-chaining, @babel/plugin-proposal-optional-chaining 'plugins' para soportar Optional Chaining Operator con Babel

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Persona = { name: 'Juan' };
+Persona ?.age;              //  Optional Chaining Operator (Comprueba si existe la propiedad en el objeto)
+Persona && Persona .age;    //  Similar a Optional Chaining Operator
+
 ReactDOM .render(
     <h2 style={{ color: "#61DBFB" }}>Hola Mundo usando React!</h2>,        // React createElement( 'h1', null, 'Hola Mundo usando React!' ),
     document .getElementById( 'app' )  
--- webpack-rules/javascript-rules.js
+++ webpack-rules/javascript-rules.js
@@ -4,7 +4,12 @@ module .exports = {
     use: {
         loader: 'babel-loader',
         options: {
-            presets: [ '@babel/preset-react' ]
+            presets: [ '@babel/preset-react' ],
+            plugins: [ 
+                /** Dos opciones de plugins para hacer uso del Optional Chaining Operator en nuestro codigo */
+                //'@babel/plugin-syntax-optional-chaining',     //  Plugin que dara soporte a la caracteristica nueva sin modificarla
+                '@babel/plugin-proposal-optional-chaining'      //  Plugin que transpilara/transformara la caracteristica nueva a codigo compatible
+            ]
         }
     }
 };
