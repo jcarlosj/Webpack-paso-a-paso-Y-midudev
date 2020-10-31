a245fdd1797507d5c928f6088299ff3eff42cbcc
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-31 16:24:13 -0500

    [+] Webpack: Configura el nombre del archivo de salida

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const javaScriptRules = require( './webpack-rules/javascript-rules' );
 
 /** Configuracion usando un Objeto */
 module .exports = {
+    output: {
+        filename: 'app.js'
+    },
     module: {
         rules: [ javaScriptRules ]
     }
