0da1af8143c3d55c39d7b3fd3446d23986624fc0
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-31 16:39:09 -0500

    [+] NPM: Installa html-webpack-plugin. Agrega hash al nombre del archivo JavaScript de salida. Configura Webpack para autogenerar archivo index.html que enlace al archivo javascript de manera automatica. Elimina archivo index.html original de ./src

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
-const javaScriptRules = require( './webpack-rules/javascript-rules' );
+const 
+    HtmlWebpackPlugin = require( 'html-webpack-plugin' ),
+    javaScriptRules = require( './webpack-rules/javascript-rules' );
 
 /** Configuracion usando un Objeto */
 module .exports = {
     output: {
-        filename: 'app.js'
+        filename: 'app.[contenthash].js'        //  Crea un contenthast al nombre del archivo para evitar tener problemas de cache
     },
     module: {
         rules: [ javaScriptRules ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin()
+    ]
 }
