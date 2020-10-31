e89ecd47adb7f9c7e5c18fd7156a053ee4e63fac
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-31 14:56:11 -0500

    [+] NPM: Instala depenencias @babel/core babel-loader @babel/preset-react. Cambia codigo de index.js no interpretable por el navegador

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM .render(
-    '<h1>Hola Mundo usando React!</h1>',        // React createElement( 'h1', null, 'Hola Mundo usando React!' ),
+    <h1>Hola Mundo usando React!</h1>,        // React createElement( 'h1', null, 'Hola Mundo usando React!' ),
     document .getElementById( 'app' )  
 );
