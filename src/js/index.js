4b548d925717414294da90eeaca54cbf14aef1f6
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-31 15:11:50 -0500

    [+] Webpack: Crea archivo de configuracion para compilar JSX usando babel-loader

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM .render(
-    <h1>Hola Mundo usando React!</h1>,        // React createElement( 'h1', null, 'Hola Mundo usando React!' ),
+    <h2 style={{ color: "#61DBFB" }}>Hola Mundo usando React!</h2>,        // React createElement( 'h1', null, 'Hola Mundo usando React!' ),
     document .getElementById( 'app' )  
 );
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+/** Configuracion usando un Objeto */
+module .exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [ '@babel/preset-react' ]
+                    }
+                }
+            }
+        ]
+    }
+}
